b0ac8a6c31b71a2b0eaf1d760e3cdcc84fbd5fca
dannis10 <dansangar11@alu.edu.gva.es>
2022-11-30 10:12:46 -0500

    Añadido en test.js nuevas operaciones aritmeticas

--- test.js
+++ test.js
@@ -1,19 +1,49 @@
 import test from 'ava';
 
-test('foo', t => {
+/*test('foo', t => {
 	t.is(2 + 2, 4);
-});
+});*/
 
+//TODO test suma
 function sum (a, b) {
   return a + b;
 }
 
-test('foo is being tested', t => {
-  t.pass();
-})
 test('sum of 2 numbers', t => {
   t.is(sum(1, 2), 3);
 })
 
+
+/*test('foo is being tested', t => {
+  t.pass();
+})*/
+
 //TODO test multiplicacion
+
+function  multiplicacion(a, b) {
+  return a * b;
+}
+
+test('multiplicacion of 2 numbers', t => {
+  t.is(multiplicacion(1, 2), 2);
+})
+
+//TODO test division
+
+function  division(a, b) {
+  return a / b;
+}
+
+test('division of 2 numbers', t => {
+  t.is(division(8, 2), 4);
+})
+
 //TODO test restar
+
+function  resta(a, b) {
+  return a - b;
+}
+
+test('resta of 2 numbers', t => {
+  t.is(resta(4, 2), 2);
+})
